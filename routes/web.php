f2fc90ad0a841c8cc553a47bf9ab39ff111c033a
StandeMan15 <99266225+StandeMan15@users.noreply.github.com>
2023-02-16 14:56:03 +0100

    start gamestore

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\PostCommentsController;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PostsController::class, 'index'])->name('home');
+
+Route::get('posts/{post:slug}', [PostsController::class, 'show']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
